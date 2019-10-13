0ea7dd7fbc8ecb61beb375317a4b54ff60cad185
abhisek11 <abhisekrock94@gmail.com>
2019-10-14 02:53:45 +0530

    app is completed

--- flask_project/auth.py
+++ flask_project/auth.py
@@ -2,7 +2,7 @@ from flask import Blueprint,render_template,redirect,url_for,flash,request
 from werkzeug.security import generate_password_hash,check_password_hash
 from . import db 
 from .models import User
-from flask_login import login_user
+from flask_login import login_user, logout_user, login_required
 
 auth = Blueprint('auth',__name__)
 
@@ -54,6 +54,7 @@ def signup_post():
     return redirect(url_for('auth.login'))
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return render_template('logout.html')
-
+    logout_user()
+    return redirect(url_for('main.index'))
--- flask_project/main.py
+++ flask_project/main.py
@@ -1,4 +1,5 @@
 from flask import Blueprint,render_template
+from flask_login import login_required, current_user
 
 from .import db 
 
@@ -9,5 +10,6 @@ def index():
     return render_template('index.html')
 
 @main.route('/profile')
+@login_required
 def profile():
     return render_template('profile.html')
